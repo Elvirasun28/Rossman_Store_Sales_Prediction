26d0a370b6a219106a1733b07cad3a92f90df90c
Elvirasun28 <elvirasun28@outlook.com>
2019-01-01 08:30:46 -0500

    Update errorMetric.py

--- main/errorMetric.py
+++ main/errorMetric.py
@@ -1,26 +1,22 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
-def train_err(X_train, y_train,model):
-    pred = model.predict(X_train)
+def train_err(train_pred, y_train):
     y_train = y_train.values
-    pred = [0 if p < 1000 else int(p) for p in pred]
-    mse = mean_squared_error(y_train, pred)
+    mse = mean_squared_error(y_train, train_pred)
     rmse = np.sqrt(mse)
-    rmsepe = np.sqrt(np.mean([((pred[i] - y_train[i]) / y_train[i])**2 for i in range(len(y_train)) if y_train[i] != 0]))
+    rmsepe = np.sqrt(np.mean([((train_pred[i] - y_train[i]) / y_train[i])**2 for i in range(len(y_train)) if y_train[i] != 0]))
     return rmse,rmsepe
 
-def val_err(X_val, y_val, model):
-    pred = model.predict(X_val)
+def val_err(val_pred, y_val):
     y_val = y_val.values
-    pred = [0 if p < 1000 else p for p in pred]
-    mse = mean_squared_error(y_val,pred)
+    mse = mean_squared_error(y_val,val_pred)
     rmse = np.sqrt(mse)
     rmsepe = np.sqrt(
-        np.mean([((pred[i] - y_val[i]) / y_val[i]) ** 2 for i in range(len(y_val)) if y_val[i] != 0]))
+        np.mean([((val_pred[i] - y_val[i]) / y_val[i]) ** 2 for i in range(len(y_val)) if y_val[i] != 0]))
     return rmse,rmsepe
 
-def err_metric(X_train,y_train,X_val, y_val, model):
-    train_error,train_rmsepe = train_err(X_train,y_train, model)
-    val_error,val_rmsepe = val_err(X_val,y_val,model)
+def err_metric(train_pred,y_train,val_pred, y_val):
+    train_error,train_rmsepe = train_err(train_pred,y_train)
+    val_error,val_rmsepe = val_err(val_pred,y_val)
     return train_error,train_rmsepe,val_error,val_rmsepe
