d8b2dec7b3a254fbdf8d6e6857e6140d15f2511f
Elvirasun28 <elvirasun28@outlook.com>
2019-01-01 08:31:16 -0500

    Update model.py

--- main/model.py
+++ main/model.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import KFold
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
 import xgboost as xgb
 import lightgbm as lgb
 import pickle
@@ -55,48 +56,38 @@ def linearreg(train,k):
 def xgb(train,k):
     print('>> The XGBoosting Model Selected ....................................\n')
     train = train.reset_index()
-    fea = train.columns.drop(['Open', 'Sales', 'PromoInterval', 'Date'])
+    fea = train.columns.drop(['index','Open', 'Sales', 'PromoInterval', 'Date'])
     train_fea = train[fea]
-    train_fea = train_fea.fillna(0)
     train_y = train.Sales
-
-    xgb_model = xgb.XGBRegressor(n_estimators=100, learning_rate=0.09, gamma=0, subsample=0.90,
-                               colsample_bytree=1, max_depth=7,random_state=SEED)
-
-    train_errs = []
-    train_rmsepes = []
-    val_errs = []
-    val_rmsepes = []
-    kf = KFold(n_splits=k, shuffle=True, random_state=SEED)
-    print('The {} folds cross validation Begins ......................\n'.format(k))
-    for train_idx, val_idx in kf.split(train_fea, train_y):
-        X_train, X_val = train_fea.iloc[train_idx], train_fea.iloc[val_idx]
-        y_train, y_val = train_y[train_idx], train_y[val_idx]
-
-        ## Because it is a tree-based algorithm, do not need  the feature scaling and build the model
-        xgb_model.fit(X_train,y_train)
-        train_error, train_rmsepe, val_error, val_rmsepe = err_metric(X_train, y_train, X_val, y_val, xgb_model)
-
-        train_errs.append(train_error)
-        train_rmsepes.append(train_rmsepe)
-        val_errs.append(val_error)
-        val_rmsepes.append(val_rmsepe)
-
-        # generate report
+    param = {'nthread': -1,
+             'max_depth': 7,
+             'eta': 0.02,
+             'silent': 1,
+             'objective': 'reg:linear',
+             'colsample_bytree': 0.7,
+             'subsample': 0.7}
+    num_round = 150
+    X_train,X_val, y_train, y_val = train_test_split(train_fea,train_y,test_size= 0.2,random_state=SEED)
+    dtrain = xgb.DMatrix(X_train, label=np.log(y_train))
+    dtest = xgb.DMatrix(X_val)
+    evallist = [(dtrain, 'train')]
+    xgb_model = xgb.train(param, dtrain, num_round,evallist)
+    ## Because it is a tree-based algorithm, do not need  the feature scaling and build the model
+    train_pred = xgb_model.predict(dtrain)
+    test_pred = xgb_model.predict(dtest)
+    train_error, train_rmsepe, val_error, val_rmsepe = err_metric(train_pred, np.log(y_train), test_pred, np.log(y_val))
+    # generate report
     print('mean(train_error): {0} |RMSEPE(train): {1} |mean(val_error): {2}|RMSEPE(val): {3}'.
-          format(round(np.mean(train_errs), 4), round(np.mean(train_rmsepes), 4),
-                 round(np.mean(val_errs), 4), round(np.mean(val_rmsepes), 4)))
-    #743|0.11031
-
+          format(train_error,train_rmsepe,val_error, val_rmsepe))
+    #mean(train_error): 6.19802319650124 |RMSEPE(train): 2.853937112772821 |mean(val_error): 0.42165806864090005|RMSEPE(val): 0.04744090366880039
 
     # save the model to disk
-    xgb_model_final = xgb_model.fit(train_fea,train_y)
     filename = 'model/finalized_xgb_model.sav'
-    pickle.dump(xgb_model_final, open(filename, 'wb'))
+    pickle.dump(xgb_model, open(filename, 'wb'))
 
-    return xgb_model_final
+    return xgb_model
 
-def lightgb_model(train,k):
+def lightgb_model(train,num_round):
     print('>> The LightingGMB Model Selected ....................................\n')
     train = train.reset_index()
     fea = train.columns.drop(['level_0','Open', 'Sales', 'PromoInterval', 'Date'])
@@ -104,7 +95,7 @@ def lightgb_model(train,k):
     train_fea = train_fea.fillna(0)
     train_y = train.Sales
 
-    params = {
+    param = {
         'boosting_type': 'gbdt',
         'objective': 'regression',
         'metric': {'l2', 'l1'},
@@ -115,47 +106,25 @@ def lightgb_model(train,k):
         'bagging_freq': 1,
         'verbose': 1
     }
-
-    train_errs = []
-    train_rmsepes = []
-    val_errs = []
-    val_rmsepes = []
-    kf = KFold(n_splits=k, shuffle=True, random_state=SEED)
-    print('The {} folds cross validation Begins ......................\n'.format(k))
-    for train_idx, val_idx in kf.split(train_fea, train_y):
-        X_train, X_val = train_fea.iloc[train_idx], train_fea.iloc[val_idx]
-        y_train, y_val = train_y[train_idx], train_y[val_idx]
-
-        ## Because it is a tree-based algorithm, do not need  the feature scaling and build the model
-        d_train = lgb.Dataset(X_train, label=y_train)
-        lightgbm_model = lgb.train(params, d_train, 100)
-        train_error, train_rmsepe, val_error, val_rmsepe = err_metric(X_train, y_train, X_val, y_val, lightgbm_model)
-
-        train_errs.append(train_error)
-        train_rmsepes.append(train_rmsepe)
-        val_errs.append(val_error)
-        val_rmsepes.append(val_rmsepe)
+    X_train, X_val, y_train, y_val = train_test_split(train_fea, train_y, test_size=0.2, random_state=SEED)
+    dtrain = lgb.Dataset(X_train, label=np.log(y_train))
+    dtest = lgb.Dataset(X_val)
+    evallist = [(dtrain, 'train')]
+    lgb_model = lgb.train(param, dtrain, num_round, evallist)
+    train_pred = lgb_model.predict(dtrain)
+    test_pred = lgb_model.predict(dtest)
+    train_error, train_rmsepe, val_error, val_rmsepe = err_metric(train_fea, y_train, test_pred, y_val)
 
         # generate report
     print('mean(train_error): {0} |RMSEPE(train): {1} |mean(val_error): {2}|RMSEPE(val): {3}'.
-          format(round(np.mean(train_errs), 4), round(np.mean(train_rmsepes), 4),
-                 round(np.mean(val_errs), 4), round(np.mean(val_rmsepes), 4)))
+          format(train_error, train_rmsepe,val_error, val_rmsepe))
 
     # mean(train_error): 553.2285 |RMSEPE(train): 0.0938 |mean(val_error): 559.4839|RMSEPE(val): 0.0971
 
     # save the model to disk
-    d_train = lgb.Dataset(train_fea, label=train_y)
-    lightgbm_model_final = lgb.train(params, d_train, 150)
     filename = 'model/finalized_lightgbm_model.sav'
-    pickle.dump(lightgbm_model_final, open(filename, 'wb'))
+    pickle.dump(lgb_model, open(filename, 'wb'))
 
-    return lightgbm_model_final
-
-
-
-
-
-
-
+    return lgb_model
 
 
